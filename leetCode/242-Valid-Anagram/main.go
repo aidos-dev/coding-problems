fa29093919c892dd1ab6fbbce217ef9127904823
aidos-dev <3300532@gmail.com>
2023-08-17 14:51:43 +0600

    solved and tested

--- leetCode/242-Valid-Anagram/main.go
+++ leetCode/242-Valid-Anagram/main.go
@@ -5,5 +5,35 @@ func main() {
 }
 
 func isAnagram(s string, t string) bool {
-	return false
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	hashChanS := make(chan map[byte]int)
+	hashChanT := make(chan map[byte]int)
+
+	go prepareHash(s, hashChanS)
+	go prepareHash(t, hashChanT)
+
+	hashS := <-hashChanS
+	hashT := <-hashChanT
+
+	for key := range hashS {
+		if hashS[key] != hashT[key] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func prepareHash(input string, hashCh chan map[byte]int) {
+	hashRes := make(map[byte]int)
+
+	for i := 0; i < len(input); i++ {
+		hashRes[input[i]] += 1
+	}
+
+	hashCh <- hashRes
 }
