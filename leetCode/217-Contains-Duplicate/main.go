3fbdeea664e78611d5ad14d61e31791efcd842bd
aidos-dev <3300532@gmail.com>
2023-08-17 09:08:21 +0600

    solved and tested

--- leetCode/217-Contains-Duplicate/main.go
+++ leetCode/217-Contains-Duplicate/main.go
@@ -6,10 +6,15 @@ func main() {
 
 func containsDuplicate(nums []int) bool {
 
-	hashSet := make(map[int]int)
+	hashSet := make(map[int]struct{})
 
 	for _, el := range nums {
+		_, exists := hashSet[el]
+		if exists {
+			return true
+		}
 
+		hashSet[el] = struct{}{}
 	}
 
 	return false
